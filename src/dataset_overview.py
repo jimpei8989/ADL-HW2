318cb133b1ff7ca51e91b24e819bbc353597eec9
Wu-Jun Pei <jimpei8989@gmail.com>
2021-05-06 13:23:40 +0800

    Update dataset overview

--- src/dataset_overview.py
+++ src/dataset_overview.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from collections import Counter
 from pathlib import Path
 
 from tokenizers.tokenizer import Tokenizer
@@ -6,13 +7,12 @@ from tokenizers.utils import concurrent_tokenize
 
 from utils.logger import logger
 from utils.io import json_load
-from utils.tqdmm import tqdmm
 
 
 def main(args):
     logger.info(args)
 
-    logger.info("Analyzing context.json")
+    logger.info("Analyzing context.json...")
     context = json_load(args.dataset_dir / "context.json")
     logger.info(f"#contexts: {len(context)}")
     logger.info(
@@ -36,10 +36,21 @@ def main(args):
                 f"mean:\t{sum(map(len, tokenized)) / len(tokenized):.2f}",
                 f"min:\t{min(map(len, tokenized))}",
                 f"max:\t{max(map(len, tokenized))}",
-                f">510:\t{sum(map(lambda t: len(t) > 510, tokenized))}/{len(context)}"
+                f">510:\t{sum(map(lambda t: len(t) > 510, tokenized))}/{len(context)}",
             ]
         )
     )
+
+    def analyze(json_name, is_private=False):
+        logger.info(f"Analyzing {json_name}...")
+        train_data = json_load(args.dataset_dir / json_name)
+        logger.info(f"#training examples: {len(train_data)}")
+        num_paragraph_counter = Counter(map(lambda d: len(d["paragraphs"]), train_data))
+        logger.info(f"About the related paragraphs: {num_paragraph_counter}")
+
+    analyze("train.json")
+    analyze("public.json")
+    analyze("private.json", is_private=True)
 
 
 def parse_arguments():
