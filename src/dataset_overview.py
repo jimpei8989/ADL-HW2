37a6cda60f2143131e7914a0c41b1bd563ef4a91
Wu-Jun Pei <jimpei8989@gmail.com>
2021-05-07 20:02:16 +0800

    Add some check in overview

--- src/dataset_overview.py
+++ src/dataset_overview.py
@@ -2,8 +2,6 @@ from argparse import ArgumentParser
 from collections import Counter
 from pathlib import Path
 
-from transformers import AutoTokenizer
-
 from utils.logger import logger
 from utils.io import json_load
 
@@ -14,30 +12,20 @@ def main(args):
     logger.info("Analyzing context.json...")
     context = json_load(args.dataset_dir / "context.json")
     logger.info(f"#contexts: {len(context)}")
+    context_lengthes = sorted(map(len, context))
     logger.info(
         "\n".join(
             [
                 "About the lengthes (character level)",
-                f"mean:\t{sum(map(len, context)) / len(context):.2f}",
-                f"min:\t{min(map(len, context))}",
-                f"max:\t{max(map(len, context))}",
+                f"mean:\t{sum(context_lengthes) / len(context):.2f}",
+                f"min:\t{min(context_lengthes)}",
+                f"max:\t{max(context_lengthes)}",
+                f"90%:\t{context_lengthes[int(len(context) * 0.9)]}",
                 f">510:\t{sum(map(lambda t: len(t) > 510, context))} / {len(context)}",
             ]
         )
     )
 
-    # tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
-    # logger.info(
-    #     "\n".join(
-    #         [
-    #             "About the lengthes (token level)",
-    #             f"mean:\t{sum(map(len, tokenized)) / len(tokenized):.2f}",
-    #             f"min:\t{min(map(len, tokenized))}",
-    #             f"max:\t{max(map(len, tokenized))}",
-    #             f">510:\t{sum(map(lambda t: len(t) > 510, tokenized))}/{len(context)}",
-    #         ]
-    #     )
-    # )
     def print_counter(counter):
         return "{\n" + "\n".join(f"  {k} -> {v}" for k, v in sorted(counter.items())) + "\n}"
 
@@ -51,6 +39,15 @@ def main(args):
 
         question_length_counter = Counter(map(lambda d: len(d["question"]) // 10, data))
         logger.info(f"About the question lengths: {print_counter(question_length_counter)}")
+
+        if not is_private:
+            assert all(
+                all(
+                    a["text"] == context[d["relevant"]][a["start"] : a["start"] + len(a["text"])]
+                    for a in d["answers"]
+                )
+                for d in data
+            )
 
     analyze("train.json")
     analyze("public.json")
