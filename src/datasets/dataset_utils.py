b03717c1fb36648e1b1ea3f54459538c9f70651c
Wu-Jun Pei <jimpei8989@gmail.com>
2021-05-09 23:49:43 +0800

    Add QA dataset

--- src/datasets/dataset_utils.py
+++ src/datasets/dataset_utils.py
@@ -1,15 +1,32 @@
+from typing import Optional
+
+NO_SPAN = -1
+
 def split_context_and_tokenize(
-    context: str, maximum_length: int, start_index: int, end_index: int, tokenizer
+    question: str,
+    context: str,
+    start_index: int,
+    end_index: int,
+    tokenizer,
+    tokenizer_max_length: int = 512,
+    add_to_dict: Optional[dict] = {}
 ):
-    fragments = []
+    # print(question, context, start_index)
+    question_tokens = tokenizer.tokenize(question)
+    maximum_length = tokenizer_max_length - 3 - len(question_tokens)
+
+    raw = []
 
     fragment_start_index = 0
     while fragment_start_index < len(context):
+        next_dot = context.find("。", fragment_start_index)
         fragment_end_index = min(
             fragment_start_index + maximum_length,
-            context.find("。", fragment_start_index) + 1,
+            next_dot + 1 if next_dot != -1 else len(context),
             len(context),
         )
+
+        # print(fragment_start_index, fragment_end_index)
 
         text = context[fragment_start_index:fragment_end_index]
         if fragment_start_index <= start_index <= end_index <= fragment_end_index:
@@ -21,12 +38,12 @@ def split_context_and_tokenize(
             label_start, label_end = len(before), len(before) + len(ans)
         else:
             tokens = tokenizer.tokenize(text)
-            label_start, label_end = -1, -1
+            label_start, label_end = NO_SPAN, NO_SPAN
 
-        fragments.append(
+        raw.append(
             {
-                "text": text,
-                "tokens": tokens,
+                "paragraph_text": text,
+                "paragraph_tokens": tokens,
                 "start_index": label_start,
                 "end_index": label_end,
             }
@@ -35,44 +52,49 @@ def split_context_and_tokenize(
 
     def merge_fragments(a, b):
         return {
-            "text": a["text"] + b["text"],
-            "tokens": a["tokens"] + b["tokens"],
+            "paragraph_text": a["paragraph_text"] + b["paragraph_text"],
+            "paragraph_tokens": a["paragraph_tokens"] + b["paragraph_tokens"],
             "start_index": (
                 a["start_index"]
-                if a["start_index"] != -1
-                else b["start_index"] + len(a["tokens"])
-                if b["start_index"] != -1
-                else -1
+                if a["start_index"] != NO_SPAN
+                else b["start_index"] + len(a["paragraph_tokens"])
+                if b["start_index"] != NO_SPAN
+                else NO_SPAN
             ),
             "end_index": (
                 a["end_index"]
-                if a["end_index"] != -1
-                else b["end_index"] + len(a["tokens"])
-                if b["end_index"] != -1
-                else -1
+                if a["end_index"] != NO_SPAN
+                else b["end_index"] + len(a["paragraph_tokens"])
+                if b["end_index"] != NO_SPAN
+                else NO_SPAN
             ),
         }
 
-    compressed_fragments = []
-    for fragment in fragments:
+    compressed = []
+    for fragment in raw:
         if (
-            len(compressed_fragments) == 0
-            or len(compressed_fragments[-1]["tokens"]) + len(fragment["tokens"]) > maximum_length
+            len(compressed) == 0
+            or len(compressed[-1]["paragraph_tokens"]) + len(fragment["paragraph_tokens"]) > maximum_length
         ):
-            compressed_fragments.append(fragment)
+            compressed.append(fragment)
         else:
-            compressed_fragments[-1] = merge_fragments(compressed_fragments[-1], fragment)
+            compressed[-1] = merge_fragments(compressed[-1], fragment)
 
-    return compressed_fragments
+    def finalize(d):
+        d.update({"question": question, "question_tokens": question_tokens, **add_to_dict})
+        return d
+
+    return [finalize(d) for d in compressed]
 
 
 if __name__ == "__main__":
     from transformers import BertTokenizer
 
+    question = "舍本和誰的數據能推算出連星的恆星的質量？"
     context = "在19世紀雙星觀測所獲得的成就使重要性也增加了。在1834年，白塞爾觀測到天狼星自行的變化，因而推測有一顆隱藏的伴星；愛德華·皮克林在1899年觀測開陽週期性分裂的光譜線時發現第一顆光譜雙星，週期是104天。天文學家斯特魯維和舍本·衛斯里·伯納姆仔細的觀察和收集了許多聯星的資料，使得可以從被確定的軌道要素推算出恆星的質量。第一個獲得解答的是1827年由菲利克斯·薩瓦里透過望遠鏡的觀測得到的聯星軌道。對恆星的科學研究在20世紀獲得快速的進展，相片成為天文學上很有價值的工具。卡爾·史瓦西發現經由比較視星等和攝影星等的差別，可以得到恆星的顏色和它的溫度。1921年，光電光度計的發展可以在不同的波長間隔上非常精密的測量星等。阿爾伯特·邁克生在虎克望遠鏡第一次使用干涉儀測量出恆星的直徑。"
     start, end = 108, 112
     print(context[start:end])
     fragments = split_context_and_tokenize(
-        context, 372, start, end, BertTokenizer.from_pretrained("bert-base-chinese")
+        question, context, start, end, BertTokenizer.from_pretrained("bert-base-chinese")
     )
     print(fragments)
---
+++ src/question_answering.py
@@ -0,0 +1,95 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+import torch
+from torch.utils.data import DataLoader
+from transformers import BertTokenizer
+
+from datasets.qa_dataset import QADataset
+# from models.qa_model import QAModel
+# from trainers.qa_trainer import QATrainer
+from utils import set_seed
+from utils.config import Config
+from utils.logger import logger
+
+
+def main(args):
+    set_seed(args.seed)
+    config = Config.load(args.config_json)
+    logger.info(f"Config: {config}")
+
+    tokenizer = BertTokenizer.from_pretrained(config.model.bert_name)
+
+    def set_seed_for_dataset_worker(worker_id):
+        set_seed(args.seed + worker_id)
+
+    def to_dataloader(dataset, **kwargs):
+        return DataLoader(
+            dataset,
+            batch_size=args.override_batch_size or config.misc.batch_size,
+            num_workers=config.misc.num_workers,
+            worker_init_fn=set_seed_for_dataset_worker,
+            **kwargs,
+        )
+
+    if args.do_train:
+        # model = ContextSelector(**config.model)
+        # trainer = ContextSelectionTrainer(
+        #     model, checkpoint_dir=config.checkpoint_dir, device=args.device, **config.trainer
+        # )
+        dataset = QADataset.from_json(
+            args.dataset_dir / "context.json",
+            args.dataset_dir / "train.json",
+            tokenizer=tokenizer,
+        )
+
+        # train_size = int(0.8 * len(dataset))
+
+        # train_split, val_split = torch.utils.data.random_split(
+        #     dataset,
+        #     [train_size, len(dataset) - train_size],
+        #     generator=torch.Generator().manual_seed(args.seed),
+        # )
+
+        # trainer.train(
+        #     to_dataloader(train_split),
+        #     to_dataloader(val_split),
+        # )
+
+    if args.do_evaluate:
+        pass
+
+    if args.do_predict:
+        pass
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("config_json", type=Path, help="Path to config json file")
+
+    # Filesystem
+    parser.add_argument("--dataset_dir", type=Path, default=Path("dataset/chineseQA"))
+    parser.add_argument("--test_json", type=Path)
+    parser.add_argument("--predict_csv", type=Path)
+
+    # Actions
+    parser.add_argument("--do_train", action="store_true")
+    parser.add_argument("--do_evaluate", action="store_true")
+    parser.add_argument("--do_predict", action="store_true")
+
+    # Misc
+    parser.add_argument("--gpu", action="store_true")
+    parser.add_argument("--seed", default=0x06902029)
+    parser.add_argument("--specify_checkpoint", type=Path)
+    parser.add_argument("--override_batch_size", type=int)
+
+    args = parser.parse_args()
+    args.device = torch.device("cuda" if args.gpu else "cpu")
+
+    assert not args.do_predict or all(a is not None for a in [args.test_json, args.predict_csv])
+
+    return args
+
+
+if __name__ == "__main__":
+    main(parse_arguments())
